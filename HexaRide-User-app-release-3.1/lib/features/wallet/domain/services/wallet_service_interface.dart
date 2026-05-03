9e93514b61f4892d3e8d84cd4bdb36905bf99444
jubairsiyum <jubairsiyum@gmail.com>
2026-05-04 01:18:38 +0600

    Refactor code structure for improved readability and maintainability

--- HexaRide-User-app-release-3.1/lib/features/wallet/domain/services/wallet_service_interface.dart
+++
@@ -1,7 +0,0 @@
-abstract class WalletServiceInterface{
-  Future<dynamic> getTransactionList(int offset);
-  Future<dynamic> getLoyaltyPointList(int offset);
-  Future<dynamic> convertPoint(String point);
-  Future<dynamic> transferWalletMoney(String balance);
-  Future<dynamic> getAddFundPromotionalList();
-}
